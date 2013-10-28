ede65030a941ae5529634808be34b189491f7a79
Theisen Sanders <theisen.sanders@gmail.com>
2013-10-28 12:55:52 -0500

    Added new color

--- color_sampling.py
+++ color_sampling.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    primary = request.args.get('primary', '333333')
-    secondary = request.args.get('secondary', 'FFFFFF')
+    primary = request.args.get('primary', '046380')
+    secondary = request.args.get('secondary', 'E6E2AF')
     reverse = request.args.get('reverse', 'false').lower() == 'true'
 
     primary_hex = '#' + primary
@@ -25,7 +25,8 @@ def parakee():
         ['334D5C', 'DF5A49'],
         ['e67e22', '16828c'],
         ['A8CD1B', '005A31'],
-        ['2ECC71', 'FF6666']
+        ['2ECC71', 'FF6666'],
+        ['046380', 'E6E2AF']
     ]
     return render_template('parakee.html', colors=colors)
 
