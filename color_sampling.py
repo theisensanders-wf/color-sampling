0e4d063b0a3b60a7dad532b6674aefcdfe18b813
Theisen Sanders <theisen.sanders@gmail.com>
2013-10-28 00:19:21 -0500

    Build custom birds

--- color_sampling.py
+++ color_sampling.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     primary = '#' + request.args.get('primary', 'FFFFFF')
@@ -10,6 +9,14 @@ def hello_world():
 
     print primary
     return render_template('index.html', primary=primary, secondary=secondary)
+
+@app.route('/parakee')
+def parakee():
+    colors = [
+        ['334D5C', 'DF5A49'],
+        ['e67e22', '16828c']
+    ]
+    return render_template('parakee.html', colors=colors)
 
 
 if __name__ == '__main__':
